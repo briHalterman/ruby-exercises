b9e85907728299b3e6986b6cfa80bda154a85b26
Brittany Halterman <halterma@gmail.com>
2025-04-27 16:55:08 -0400

    Use p to find and fix the error on line 40

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -37,8 +37,11 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  string_array = string.downcase.split("")
+  p string_array
   unique_length = string_array.uniq.length
+  p "unique_length: #{unique_length}"
+  p "original_length: #{original_length}"
   original_length == unique_length
 end
 
